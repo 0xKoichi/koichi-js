f54386f52d897baef23eef804e3330d59fb69cba
koichi <biccboii@protonmail.com>
2021-12-17 21:33:42 +0000

    music command – forgot to rename slash command data

--- src/commands/fun/music.js
+++ src/commands/fun/music.js
@@ -2,10 +2,8 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const musicInit = require("../../components/fun/music/music-handler");
 
 const data = new SlashCommandBuilder()
-  .setName("activity")
-  .setDescription(
-    "Set up activities with your friends! (Must be in Voice Channel)"
-  );
+  .setName("music")
+  .setDescription("Start streaming music!");
 
 module.exports = {
   data,
