b7e76df1c494abb97dc90e544141559cf942907d
alsheikhiu <alsheikh@iu.edu>
2021-12-17 18:05:45 -0500

    eeeeeeeeee

--- src/components/economy/user-progress.js
+++ src/components/economy/user-progress.js
@@ -1,7 +1,9 @@
 const { filledBar } = require("string-progressbar");
 
 module.exports = getProgress = (user) => {
-  xpNeeded = Math.round(user.s_LVL * 1000 * Math.exp(user.s_LVL / 50));
+  const exponent = 1.5;
+  const baseXP = 1000;
+  const xpNeeded = Math.floor(baseXP * Math.pow(user.s_LVL, exponent));
   const percentage = Math.round((user.xp / xpNeeded) * 100);
   const progress = filledBar(100, percentage, 20);
   return progress[0];
