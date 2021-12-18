c1c2a498dc64adcf0aa9c4e2c8f5c166ab8491a4
koichi <biccboii@protonmail.com>
2021-12-18 10:19:55 +0000

    check if guild has community feature – if not, make it'

--- src/components/fun/music/music-handler.js
+++ src/components/fun/music/music-handler.js
@@ -3,7 +3,7 @@ const ytdl = require("ytdl-core");
 const Queue = require("./queue");
 
 const musicInit = async (interaction, client) => {
-  console.log(interaction, client);
+  console.log(interaction);
   await queueHandler(interaction, client);
 };
 
@@ -18,7 +18,14 @@ const stageHandler = async (interaction, client) => {
   const guild = await interaction.member.guild.fetch();
   const member = await guild.members.fetch(interaction.user.id);
 
-  console.log(guild.features);
+  if (!guild.features.includes("COMMUNITY")) {
+    guild
+      .edit({
+        features: ["COMMUNITY"],
+      })
+      .then((updated) => console.log(`New guild features ${updated}`))
+      .catch(console.error);
+  }
 };
 
 module.exports = musicInit;
